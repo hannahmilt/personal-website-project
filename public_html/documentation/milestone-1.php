bdd92206f17a163112216f39b5234619df31c0f0
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-03-16 22:24:34 -0600

    Created directory public_html Created directory public_documentation inside public_html Created file milestone-1.php inside documentation directory Outlined html format with names title sections
    *Not quite sure how to update README.md file. looks like it already has a disciption from what I put in github

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> Personal Website</title>
+</head>
+<body>
+	<h1>Purpose:</h1>
+		<p></p>
+	<h1>Audience:</h1>
+		<p></p>
+	<h1>Goal:</h1>
+		<p></p>
+	<div>
+		<h1>Persona:</h1>
+			<h2>Name: Clara Burbridge</h2>
+			<h2>Background;</h2>
+			<h2>Goals:</h2>
+
+	</div>
+</body>
+
+</html>
