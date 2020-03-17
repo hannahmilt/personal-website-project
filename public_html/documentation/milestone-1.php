d53d7e7574e5858d0f9d600369c29b87440adc21
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-03-17 17:01:05 -0600

    Created directory public_html Created directory public_documentation inside public_html Created file milestone-1.php inside documentation directory Outlined html format with names title sections
    *Not quite sure how to update README.md file. looks like it already has a disciption from what I put in github
    
    Started entering phase one: Purpose, Audience, Goal, and Creating a Persona.
    *Need to add picture for Persona.
    
    Added picture and typed in more in discription

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -13,13 +13,15 @@
 		<p>Create a Portfolio of my work to acquire full-time employment or an internship.</p>
 	<div>
 		<h1>Persona:</h1>
-		<img src=""
+		<img src= "pwp-image1.jpg" alt="profile picture">
 			<h2>Name: Clara Burbridge</h2>
 			<h2>Background:</h2>
-				<p>Clara is a hiring manager for Presbyterian Heathcare Services IT department. She has been working for Presbyterian for 20 years.
-					She is 55years old and has plenty of experience with technology.
-					She is tasked with looking over applicants application and </p>
+				<p>Clara is a hiring manager for Presbyterian Heathcare Services IT department. She has been working for Presbyterian for 6 years.
+					She is 35years old and has plenty of experience with technology. Her personal computer is a Macbook Air iOS 13.3 and has Iphone 8.
+					But if it is work related she uses her desktop in her office which is a Dell Inspiron Business Desktop, 9th gen Intel Core 8GB.
+					 </p>
 			<h2>Goals:</h2>
+				<p>She is tasked with looking over applicants application. She only has a limited amount of time to do this.</p>
 
 	</div>
 </body>
