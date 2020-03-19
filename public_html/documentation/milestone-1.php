9c7d45b2de59864de137794e4e2bd2b89eb62b9b
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-03-19 16:00:16 -0600

    Created directory public_html Created directory public_documentation inside public_html Created file milestone-1.php inside documentation directory Outlined html format with names title sections
    *Not quite sure how to update README.md file. looks like it already has a disciption from what I put in github
    
    Started entering phase one: Purpose, Audience, Goal, and Creating a Persona.
    *Need to add picture for Persona.
    
    Added picture and typed in more in discription
    
    Finish Persona discription. Checked in html in W3 site.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -17,11 +17,15 @@
 			<h2>Name: Clara Burbridge</h2>
 			<h2>Background:</h2>
 				<p>Clara is a hiring manager for Presbyterian Heathcare Services IT department. She has been working for Presbyterian for 6 years.
-					She is 35years old and has plenty of experience with technology. Her personal computer is a Macbook Air iOS 13.3 and has Iphone 8.
-					But if it is work related she uses her desktop in her office which is a Dell Inspiron Business Desktop, 9th gen Intel Core 8GB.
-					 </p>
+					She is 35 years old and has plenty of experience with technology. Clara has a strong passion for her job and is dedicated to her work.
+					She has a friendly disposition and gets along well with most people.
+
+		<p><strong>Technology:</strong> Her personal computer is a Macbook Air iOS 13.3 and has Iphone 8. But if it is work related she uses her desktop in her office which is a Dell Inspiron Business Desktop, 9th gen Intel Core 8GB.</p>
+		<p><strong>Situation:</strong></p>She is tasked with looking over applicants application. Choosing who should be brought in for an in person interview. She only has a limited amount of time to do this. A she doesn't look at any applications that don't have a portfolio of their work attached. She spends about 3 minutes on average looking each portfolio.
+		<p><strong>Frustrations:</strong> Has a limited amount of time to look over applications. Will have to be in her office to do this and may be distracted by other work, or coworkers.  </p>
 			<h2>Goals:</h2>
-				<p>She is tasked with looking over applicants application. She only has a limited amount of time to do this.</p>
+				<p>Figure out who is qualified, as well as who would be a good fit for the position to bring in for an interview. Wants to find a self motivated person who works well with a team as well as on their own.
+				As well as someone who demonstrates creativity and knowledge of user functionality in their works.</p>
 
 	</div>
 </body>
