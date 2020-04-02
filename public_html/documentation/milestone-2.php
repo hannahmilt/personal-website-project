760a004f887e73ea3102c8dfd23910cd117dee6c
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-01 23:49:33 -0600

    Created directory public_html Created directory public_documentation inside public_html Created file milestone-1.php inside documentation directory Outlined html format with names title sections
    *Not quite sure how to update README.md file. looks like it already has a disciption from what I put in github
    
    Started entering phase one: Purpose, Audience, Goal, and Creating a Persona.
    *Need to add picture for Persona.
    
    Added picture and typed in more in discription
    
    Finish Persona discription. Checked in html in W3 site.
    Typo fix
    
    Added milestone-2.php file. Created basic HTML format. Created My content Strategy for website. Linked wire frames for full layout as well as mobile layout.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+			<meta charset="UTF-8"/>
+			<title>Milestone-2</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+			<p> My site will be one page, and will consist of the following sections.</p>
+			<h2>Introduction</h2>
+				<p>Brief Hello. This section will state my name and my role as a Web Developer. This section will have a picture
+				as the background. Feature idea: for the background picture I want to see if I can get it to pop over the frame to look more
+				3D (plant) or just have Sandia mountains for background hazy or translucent</p>
+			<h2>Directory Bar</h2>
+				<p>Will have list of sections (About Me, Skills, Portfolio, Connect) with links
+				so you can jump to said labeled  section. Feature idea: As you hover over section it will either pop out more or highlight
+				to a different color.</p>
+			<h2>Information About Me</h2>
+				<p>This Section will detail my professional learning history (coding-bootcamp), core skill set. Will highlight strengths and
+				personality. On the left side I will include a picture of me. On the right side will be text. Feature idea: Would like the picture
+				 to be shaped like a circle with a border.</p>
+			<h2>My Skills</h2>
+				<p>Re-highlight skills with a bar sliding graphic labeling Html, Php, Css, and MySQL. (not really sure if i need this. noticed
+				a lot of the portfolios had this in them. Feature idea: Background should be similar to introduction.</p>
+			<h2>Portfolio</h2>
+				<p>Image gallery containing examples of my work (my not have anything to put in this in beginning). Feature idea: pictures
+				pop out slightly when mouse hovers over them.</p>
+			<h2>Connect With Me</h2>
+				<p> Icon graphics that link to my github, Linkdin, and Email. So employers can reach out to me. Feature idea: would like the icons
+				to pop up a web page link, or separate pop up for mail. Background should be similar to introduction.</p>
+		<div>
+		<h1>Full Screen Layout</h1>
+			<img src="wireframe1.png" alt="Picture of wire frame for full screens">
+		</div>
+		<div>
+		<h1>Mobile Screen Layout</h1>
+			<img src="wireframphone.png" alt="Picture of wire frame for mobile screens">
+		</div>
+	</body>
+</html>
