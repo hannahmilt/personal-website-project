9963ca3522db38def0eb13c9f1cbb2c18d9984b5
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-21 14:47:04 -0600

    added corect id in form-validate

--- public_html/js/form-validate.js
+++ public_html/js/form-validate.js
@@ -25,14 +25,14 @@ $(document).ready(function(){
 		// rules here define what is good or bad input
 		// each rule starts with the form input element's NAME attribute
 		rules: {
-			name: {
+			contactName: {
 				required: true
 			},
-			email: {
+			contactEmail: {
 				email: true,
 				required: true
 			},
-			message: {
+			contactMessage: {
 				required: true,
 				maxlength: 2000
 			}
@@ -40,14 +40,14 @@ $(document).ready(function(){
 
 		// error messages to display to the end user when rules above don't pass
 		messages: {
-			name: {
+			contactName: {
 				required: "Please enter your name."
 			},
-			email: {
+			contactEmail: {
 				email: "Please enter a valid email address.",
 				required: "Please enter a valid email address."
 			},
-			message: {
+			contactMessage: {
 				required: "Please enter a message.",
 				maxlength: "2000 characters max."
 			}
@@ -55,9 +55,9 @@ $(document).ready(function(){
 
 		// AJAX submit the form data to back end if rules pass
 		submitHandler: function(form) {
-			$("my-contact-form").ajaxSubmit({
+			$("#my-contact-form").ajaxSubmit({
 				type: "POST",
-				url: $("my-contact-form").attr("action"),
+				url: $("#my-contact-form").attr("action"),
 
 				success: function(ajaxOutput) {
 					// clear the output area's formatting
@@ -68,7 +68,7 @@ $(document).ready(function(){
 
 					// reset the form if it was successful
 					if($(".alert-success").length >= 1) {
-						$("my-contact-form")[0].reset();
+						$("#my-contact-form")[0].reset();
 					}
 				}
 			})
