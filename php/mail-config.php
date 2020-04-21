3472e56d612ad0a1ccd78786f247864f749ae94a
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-04-21 13:57:57 -0600

    added code to mail config and mailer added api and dormain keys

--- php/mail-config.php
+++ php/mail-config.php
@@ -16,19 +16,19 @@
 
 //mailgun API Key authorization
 
-$mailgunApiKey = "-- MAILGUN API KEY --";
-$mailgunDomain = " --MAILGUN DOMAIN NAME --";
+$mailgunApiKey = "14d4835a9cb816f2369aa38b165ad5c9-f135b0f1-6bf38e5d";
+$mailgunDomain = "sandboxb823ce2002cc466891918e6f1b226f7d.mailgun.org";
 
 
 // your Google reCAPTCHA keys here
 // your Google reCAPTCHA keys here
-$siteKey = '-- reCAPTCHA SITE KEY --';
-$secret = '-- reCAPTCHA SECRET KEY --';
+$siteKey = '6LekBewUAAAAALedoCSq-5XmMzmSO2efs1KmZ2GL';
+$secret = '6LekBewUAAAAAJItN715iFQ2eJ6rC_azdoGvuJxD';
 
 /**
  * attach the recipients to the message
  * notice this an array that can include or omit the the recipient's real name
  * use the recipients' real name where possible; this reduces the probability of the Email being marked as spam
  **/
-$MAIL_RECIPIENTS = [ "mail@email.com" => "Recipient Name"];
-$MAIL_RECIPIENT = ["mail@mail.com"];
+$MAIL_RECIPIENTS = [ "hannahmilt@gmail.com" => "Hannah Miltenberger"];
+$MAIL_RECIPIENT = ["hannahmilt@gmail.com"];
